3dfff226a4428f1d0b1f9fb8dc94d143e6857cb2
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-04-28 22:03:11 -0300

    Feat: Initial configs

---
+++ main.c
@@ -0,0 +1,45 @@
+
+#include "src/lib/raylib.h"
+
+int main(void)
+{
+
+    const int screenWidth = 1200;
+    const int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);              
+
+    while (!WindowShouldClose())   
+    {
+        if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
+ 		{
+ 			int display = GetCurrentMonitor();
+            
+            if (IsWindowFullscreen())
+            {
+                SetWindowSize(screenWidth, screenHeight);
+            }
+            else
+            {
+                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+            }
+
+ 			ToggleFullscreen();
+ 		}
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window!", 220, 200, 20, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();        
+
+    return 0;
+}
