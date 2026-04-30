7bf2041df62a1dd20a9d2a9dc81d0af736f845bf
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-04-29 21:30:28 -0300

    feat: Criando sistema de criar quadrados(classes) quando clicar, sistema de zoom usando camera2d

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
-
 #include "src/lib/raylib.h"
+#include <stdlib.h>
+#include <math.h>
+
+void checkClick(Camera2D camera)
+{
+    static int clickCount = 0;
+    static Vector2 *arrayPositions = NULL;
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        clickCount++; 
+
+        arrayPositions = (Vector2 *) realloc(arrayPositions, clickCount * sizeof(Vector2));
+
+        arrayPositions[clickCount - 1] = GetScreenToWorld2D(GetMousePosition(), camera);
+    }
+
+    for (int i = 0; i < clickCount; i++)
+        {
+            DrawRectangleLines(arrayPositions[i].x, arrayPositions[i].y, 100, 40, BLACK);
+        }
+}
 
 int main(void)
 {
@@ -7,12 +28,22 @@ int main(void)
     const int screenWidth = 1200;
     const int screenHeight = 800;
 
+    Camera2D camera = {0};
+    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);              
 
     while (!WindowShouldClose())   
     {
+         camera.zoom = expf(logf(camera.zoom) + ((float)GetMouseWheelMove()*0.1f));
+
+         if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
         if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
  		{
  			int display = GetCurrentMonitor();
@@ -32,10 +63,14 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
+            
+            BeginMode2D(camera);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-            DrawText("Congrats! You created your first window!", 220, 200, 20, RED);
-            DrawText("Congrats! You created your first window!", 220, 200, 20, RED);
+            checkClick(camera);
+
+            EndMode2D();
+
+            
 
         EndDrawing();
     }
