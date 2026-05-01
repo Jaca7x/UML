e6e3b1a087e4e2fdf82b75c3be9eb5be2d86d04e
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-05-01 13:33:33 -0300

    refactor: Atualizando config do "live serve"

--- build.bat
+++ build.bat
@@ -1,22 +1,24 @@
 @echo off
-:: 1. FORÇA o uso exclusivo da pasta bin da Raylib no topo do sistema
+
 set PATH=C:\raylib\w64devkit\bin;%PATH%
 
-:: 2. Configura os caminhos
 set RAYLIB_PATH=C:/raylib/raylib
 
-:: 3. Fecha o jogo se ele estiver aberto
 taskkill /F /IM game.exe /T >nul 2>&1
 
-:: 4. Compila o projeto (Note que agora uso apenas 'gcc' pois ele ja esta no PATH acima)
-echo Compilando...
-gcc main.c -o game.exe -Wall -std=c99 -DPLATFORM_DESKTOP -I. -I%RAYLIB_PATH%/src -L%RAYLIB_PATH%/src -lraylib -lopengl32 -lgdi32 -lwinmm -lshell32 -luser32
+echo Compilando RayUML...
 
-:: 5. Se a compilação deu certo, roda o jogo
+gcc main.c src/modules/*.c  -o game.exe ^
+    -Wall -std=c99 -DPLATFORM_DESKTOP ^
+    -I. -I./src/include -I%RAYLIB_PATH%/src ^
+    -L%RAYLIB_PATH%/src ^
+    -lraylib -lopengl32 -lgdi32 -lwinmm -lshell32 -luser32
+
 if %errorlevel% equ 0 (
-    echo Sucesso! Abrindo jogo...
+    cls
+    echo [SISTEMA] Compilado com sucesso! Abrindo...
     game.exe
 ) else (
     echo.
-    echo [ERRO DE COMPILACAO] Verifique o codigo acima.
+    echo [SISTEMA] ERRO DE COMPILACAO. Verifique o codigo acima.
 )
