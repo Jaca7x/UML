d12aa8af55376ad0436f534ae4c50d8ffdc926be
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-05-03 21:19:53 -0300

    feat: Criando struct das classes

--- src/include/editor.h
+++ src/include/editor.h
@@ -2,6 +2,23 @@
 #define EDITOR_H
 #include "../../lib/raylib.h"
 
-void UpdateAndDrawBoxes(Camera2D camera);
+typedef struct 
+{
+
+    Rectangle bounds;
+
+    int id;
+
+    char name[64];
+    char atributes[256];
+    char methods[256];
+
+    bool isSelected;
+    bool isDragging;
+    Vector2 dragOffSet;
+}UMLClass;
+
+void AddUMLClass (Camera2D camera);
+void UpdateAndDrawBoxes(Camera2D camera, int *cursor);
 
 #endif
