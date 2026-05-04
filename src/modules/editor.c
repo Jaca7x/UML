c90af43935b0d9978a682852986cccc5bc098c06
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-05-03 21:20:51 -0300

    feat: fazendo sistema de criar classes apartir do botão, e de arrastar as classes

--- src/modules/editor.c
+++ src/modules/editor.c
@@ -1,17 +1,67 @@
 #include "../include/editor.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-void UpdateAndDrawBoxes(Camera2D camera) {
-    static int clickCount = 0;
-    static Vector2 *arrayPositions = NULL;
+static int clickCount = 0;
+static UMLClass *arrayClass = NULL;
 
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        clickCount++;
-        arrayPositions = (Vector2 *)realloc(arrayPositions, clickCount * sizeof(Vector2));
-        arrayPositions[clickCount - 1] = GetScreenToWorld2D(GetMousePosition(), camera);
-    }
 
+void AddUMLClass(Camera2D camera)
+{    
+    Vector2 worldPos = GetScreenToWorld2D(camera.target, camera);
+
+    clickCount++;
+    arrayClass = (UMLClass *)realloc(arrayClass, clickCount * sizeof(UMLClass));
+    arrayClass[clickCount - 1].bounds.x / 2.0f - worldPos.x;
+    arrayClass[clickCount - 1].bounds.y / 2.0f - worldPos.y;
+        
+    arrayClass[clickCount - 1].bounds.width = 150;
+    arrayClass[clickCount - 1].bounds.height = 100;
+
+    arrayClass[clickCount - 1].isDragging = false;
+    arrayClass[clickCount - 1].isSelected = false;
+}
+
+void UpdateAndDrawBoxes(Camera2D camera, int *cursor) {
+    Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), camera);
+    
+    for (int i = clickCount - 1; i >= 0; i--)
+    {
+        if (CheckCollisionPointRec(mousePos, arrayClass[i].bounds))
+        {
+            *cursor = MOUSE_CURSOR_POINTING_HAND;
+
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                arrayClass[i].isDragging = true;  
+                arrayClass[i].dragOffSet.x = mousePos.x - arrayClass[i].bounds.x;
+                arrayClass[i].dragOffSet.y = mousePos.y - arrayClass[i].bounds.y;
+                break; 
+            } 
+        }
+    } 
+    
     for (int i = 0; i < clickCount; i++) {
-        DrawRectangleLines(arrayPositions[i].x, arrayPositions[i].y, 100, 40, BLACK);
+        Color colorClass = BLACK;
+
+        if (arrayClass[i].isDragging)
+        {   
+            *cursor = MOUSE_CURSOR_RESIZE_ALL;
+            colorClass = BLUE;
+
+            arrayClass[i].bounds.x = mousePos.x - arrayClass[i].dragOffSet.x;
+            arrayClass[i].bounds.y = mousePos.y - arrayClass[i].dragOffSet.y;  
+        }
+
+        arrayClass[i].id = i;
+        
+        DrawRectangleLinesEx(arrayClass[i].bounds, 2, colorClass);
+        DrawText("Nova Classe", arrayClass[i].bounds.x + 30, arrayClass[i].bounds.y + 10, 16, RED); 
     }
+
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        for (int i = 0; i < clickCount; i++) {
+            arrayClass[i].isDragging = false;
+        }
+    }  
 }
